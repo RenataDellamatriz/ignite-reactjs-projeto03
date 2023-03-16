12b4626f932e50bc41839bb8fa53509c1514948b
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-16 18:31:25 -0300

    feat: new transaction modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,25 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "phosphor-react";
+import { CloseButton, Content, Overlay } from "./styles";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <CloseButton >
+          <X size={24}/>
+        </CloseButton>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <form action="">
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
+}
