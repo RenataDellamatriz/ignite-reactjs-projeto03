5d847f8b9a595ace90e14bf596a43d4eea3c0ebb
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-16 16:37:52 -0300

    feat:setup, header and summary

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const SummaryContainer = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+
+  margin-top: -5rem;
+`;
+
+export const SummaryCard = styled.div`
+  background-color: ${(props) => props.theme["gray-600"]};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+
+    @media (max-width: 768px) {
+      font-size: 1.5rem;
+    }
+  }
+`;
