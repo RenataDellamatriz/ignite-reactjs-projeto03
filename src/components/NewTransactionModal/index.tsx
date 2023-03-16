fd69fb3a89a9853f97f1303296bcad99069fdd43
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-03-16 18:54:15 -0300

    feat: radio group

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransationTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -8,14 +14,25 @@ export function NewTransactionModal() {
       <Overlay />
 
       <Content>
-        <CloseButton >
-          <X size={24}/>
+        <CloseButton>
+          <X size={24} />
         </CloseButton>
         <Dialog.Title>Nova transação</Dialog.Title>
         <form action="">
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransationTypeButton variant="income" value="income">
+              <ArrowCircleUp />
+              Entrada
+            </TransationTypeButton>
+            <TransationTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown />
+              Saída
+            </TransationTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
